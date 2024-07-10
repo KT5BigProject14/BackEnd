a474cc38be16fe0fe9bfa947804801ff2b00edc8
moon <140053617+Moonbyeol@users.noreply.github.com>
2024-07-10 11:16:00 +0900

    ai_chat update

--- api/routes/ai.py
+++ api/routes/ai.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse, StreamingResponse
 from api.deps import GetCurrentUser
 from api.deps import JWTAuthentication
 from typing import Annotated
@@ -62,18 +62,71 @@ async def chat(request: Request):
             response = await client.post(
                 "http://localhost:8080/chat",
                 json={'user_email': user_email,
-                      'session_id': session_id, 'question': question}
+                      'session_id': session_id, 'question': question},
+                timeout=None
             )
 
         response.raise_for_status()
 
-        # 모델 서버로부터 결과 받기
-        result = response.json()
+        # 스트리밍 응답 처리
+        async def iter_response():
+            async for chunk in response.aiter_bytes():
+                yield chunk
 
-        return JSONResponse(content={"session_id": result['session_id'], "generated_chat": result["response"]})
+        return StreamingResponse(iter_response(), media_type="application/json")
+
     except httpx.RequestError as e:
         raise HTTPException(status_code=500, detail=str(e))
     except httpx.HTTPStatusError as e:
         raise HTTPException(status_code=e.response.status_code, detail=str(e))
     except KeyError as e:
         raise HTTPException(status_code=400, detail=f"Missing field: {e}")
+
+# input test하는 버전
+# @router.post("/chat")
+# async def chat(session_id: str, user_email: str, question: str):
+#     try:
+#         response = requests.post(
+#             "http://localhost:8080/chat", json={'user_email': user_email, 'session_id': session_id, 'question': question}
+#         )
+#         response.raise_for_status()
+
+#         # 랭서브로부터 결과 받기
+#         result = response.json()
+
+#         return {"session_id": result['session_id'], "generated_chat": result["response"]}
+#     except requests.exceptions.RequestException as e:
+#         raise HTTPException(status_code=500, detail=str(e))
+
+
+# stram 안하는 버전
+# @router.post("/chat")
+# async def chat(request: Request):
+#     try:
+#         # 요청 본문을 JSON 형식으로 파싱
+#         data = await request.json()
+#         session_id = data['session_id']
+#         user_email = data['user_email']
+#         question = data['question']
+
+#         # 모델 서버로 요청 보내기
+#         async with httpx.AsyncClient(timeout=None) as client:
+#             response = await client.post(
+#                 "http://localhost:8080/chat",
+#                 json={'user_email': user_email,
+#                       'session_id': session_id, 'question': question}
+#             )
+
+#         response.raise_for_status()
+
+#         # 모델 서버로부터 결과 받기
+#         result = response.json()
+
+#         return JSONResponse(content={"session_id": result['session_id'], "generated_chat": result["response"]})
+
+#     except httpx.RequestError as e:
+#         raise HTTPException(status_code=500, detail=str(e))
+#     except httpx.HTTPStatusError as e:
+#         raise HTTPException(status_code=e.response.status_code, detail=str(e))
+#     except KeyError as e:
+#         raise HTTPException(status_code=400, detail=f"Missing field: {e}")
